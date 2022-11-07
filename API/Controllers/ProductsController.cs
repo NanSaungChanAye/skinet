6b1211e4f8f9124a6ba07acfbf3b479e107538d8
Nan Saung Chan Aye <nansaungchanaye1991@gmail.com>
2022-11-07 12:50:12 +0630

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Data;
+using System.Collections.Generic;
+using Core.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController :ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context=context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+
+            //return "This will be a list of products!";
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Product>>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+        
+    }
+}
