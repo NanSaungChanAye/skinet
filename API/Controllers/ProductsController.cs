c97187a1f78b47072d89a1560d78cca6da43327d
Nan Saung Chan Aye <nansaungchanaye1991@gmail.com>
2022-11-18 10:05:16 +0630

    End of Section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Core.Entities;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -12,18 +13,18 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController :ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context=context;
+            _repo= repo;
         }
 
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
 
             //return "This will be a list of products!";
@@ -32,8 +33,22 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<List<Product>>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _repo.GetProductByIdAsync(id);
             return Ok(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var productBrands=await _repo.GetProductBrandsAsync();
+            return Ok(productBrands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            var productTypes=await _repo.GetProductBrandsAsync();
+            return Ok(productTypes);
         }
         
     }
--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            //Applying the migrations and creating the Database at app startup
+            var host=CreateHostBuilder(args).Build();
+            using (var scope=host.Services.CreateScope())
+            {
+                var services=scope.ServiceProvider;
+                var loggerFactory=services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context=services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context,loggerFactory);
+                }
+                catch (System.Exception ex)
+                {
+                    
+                    var logger=loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex,"An error occured during migration!");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
